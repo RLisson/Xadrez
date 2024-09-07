6b6bd9ce4e9e4dd22429c1fa3c5bf639a553bbee
Lucas Risson <lucasrisson@gmail.com>
2024-09-07 18:21:05 -0300

    Começando implementação do tabuleiro

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QntdMovimentos {  get; protected set; }
+        public Tabuleiro Tab {  get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Tab = tabuleiro;
+            Cor = cor;
+            QntdMovimentos = 0;
+        }
+
+    }
+}
