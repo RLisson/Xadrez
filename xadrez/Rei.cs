0aee5483d437e7ccd1aab859ff492c3b4ee91a8d
Lucas Risson <lucasrisson@gmail.com>
2024-09-15 18:35:14 -0300

    Jogada especial roque pequeno e roque grande

--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -4,12 +4,23 @@ namespace ProjetoXadrez.xadrez
 {
     internal class Rei : Peca
     {
-        public Rei(Cor cor, Tabuleiro tabuleiro)
-            : base(cor, tabuleiro) { }
+        private readonly PartidaDeXadrez _partida;
+
+        public Rei(Cor cor, Tabuleiro tabuleiro, PartidaDeXadrez partida)
+            : base(cor, tabuleiro)
+        {
+            _partida = partida;
+        }
 
         public override string ToString()
         {
             return "R";
+        }
+
+        private bool TesteTorreParaRoque(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p != null && p is Torre && p.Cor == Cor && QntdMovimentos == 0;
         }
 
         public override bool[,] MovimentosPossiveis()
@@ -57,20 +68,48 @@ namespace ProjetoXadrez.xadrez
             if (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
-            }            
-            
+            }
+
             // esquerda
             pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
             if (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
             }
-                        
+
             // noroeste
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
             if (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // #jogadaespecial roque
+            if (QntdMovimentos == 0 && !_partida.Xeque)
+            {
+                // #jogadaespecial roque pequeno
+                Posicao postT1 = new Posicao(Posicao.Linha, Posicao.Coluna + 3);
+                if (TesteTorreParaRoque(postT1))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna + 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna + 2);
+                    if (Tab.Peca(p1) == null & Tab.Peca(p2) == null)
+                    {
+                        mat[Posicao.Linha, Posicao.Coluna + 2] = true;
+                    }
+                }
+                // #jogadaespecial roque grande
+                Posicao postT2 = new Posicao(Posicao.Linha, Posicao.Coluna - 4);
+                if (TesteTorreParaRoque(postT2))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna - 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna - 2);
+                    Posicao p3 = new Posicao(Posicao.Linha, Posicao.Coluna - 3);
+                    if (Tab.Peca(p1) == null & Tab.Peca(p2) == null & Tab.Peca(p3) == null
+                    {
+                        mat[Posicao.Linha, Posicao.Coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
